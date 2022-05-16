8b89fde29bae6b6a2d3ffcde815d46871e99c65b
meda997 <medatejaswi7@gmail.com>
2022-05-16 21:17:46 +0530

    [tejaswi]:added code to calculate daily wage of employee

--- Employeewageprogram/Employeewageprogram/Program.cs
+++ Employeewageprogram/Employeewageprogram/Program.cs
@@ -9,7 +9,8 @@ namespace EmployeePresentAbsent
     public class Program
     {
         public int EmpPresent = 1;
-
+        public int EmpHr = 8;
+        public int WagesPerHr = 20;
         public void CheckEmpPresentAbsent()
         {
             Random check = new Random();
@@ -18,6 +19,8 @@ namespace EmployeePresentAbsent
             if (EmpPresent == CheckEmp)
             {
                 Console.WriteLine("Employee is Present");
+                int DailyWagePerHr = EmpHr * WagesPerHr;
+                Console.WriteLine("Dailywages :" + DailyWagePerHr);
             }
             else
             {
