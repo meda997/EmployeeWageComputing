3683e5d4d38ed721b0efa3eb566a015cbc53975b
meda997 <medatejaswi7@gmail.com>
2022-05-16 21:04:51 +0530

    [tejaswi]:added code to check employee present or absent

---
+++ Employeewageprogram/Employeewageprogram/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeePresentAbsent
+{
+    public class Program
+    {
+        public int EmpPresent = 1;
+
+        public void CheckEmpPresentAbsent()
+        {
+            Random check = new Random();
+            int CheckEmp = check.Next(0, 2);
+
+            if (EmpPresent == CheckEmp)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+        public static void Main(string[] args)
+        {
+            Program pg = new Program();
+            pg.CheckEmpPresentAbsent();
+        }
+    }
+}
